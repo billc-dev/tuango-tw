d49f01155cfce8888b5fb0c3ce5a0b342dd4fab3
billchengdev <chengbill0418@gmail.com>
2022-01-25 10:14:06 +0800

    feat: create post view

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,15 +4,11 @@ import Link from "next/link";
 const Home: NextPage = () => {
   const { data, error, isLoading } = useGetPostsQuery(1);
   console.log(data);
+  console.log("error", error);
 
   return (
     <div className="bg-indigo-400">
       <Link href="/posts">Posts</Link>
-      {data?.map((post) => (
-        <div key={post._id}>
-          #{post.postNum} {post.title} {post.displayName}
-        </div>
-      ))}
     </div>
   );
 };
--- frontend/pages/posts.tsx
+++ frontend/pages/posts.tsx
@@ -6,12 +6,32 @@ const Posts: NextPage = () => {
   console.log(data);
 
   return (
-    <div className="bg-indigo-400">
-      {data?.map((post) => (
-        <div key={post._id}>
-          #{post.postNum} {post.title} {post.displayName}
+    <div className="flex justify-center bg-slate-200">
+      <div className="max-w-4xl">
+        <div className="gap-2 p-2 grid grid-cols-2  sm:grid-cols-3 md:grid-cols-4">
+          {data?.map((post) => (
+            <div
+              key={post._id}
+              className="flex flex-col bg-white rounded-3xl overflow-hidden shadow-sm"
+            >
+              <img
+                src={post.imageUrls[0].sm}
+                className="max-h-48 h-2/3 object-cover"
+              />
+              <div className="px-2 pt-2 pb-3">
+                <div className="truncate">{post.title}</div>
+                <div className="truncate text-xs">{post.displayName}</div>
+                <div className="flex justify-between pt-2">
+                  <div className="w-2/5 truncate">
+                    {post.orderCount ? `${post.orderCount + 100} 訂單` : ""}
+                  </div>
+                  <div className="truncate">$100~$5000</div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
