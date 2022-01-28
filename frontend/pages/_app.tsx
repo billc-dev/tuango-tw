1528d45ede30423a950ec2afb277cadb6b672de7
billchengdev <chengbill0418@gmail.com>
2022-01-28 20:10:51 +0800

    fix: axios base url

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,11 +7,13 @@ import type { AppProps } from "next/app";
 import { useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { IS_DEV } from "utils/constants";
 import "../styles/globals.css";
 // ANALYZE=true yarn build
 
-axios.defaults.baseURL =
-  "http://localhost:5000/tuango-tw-firebase/asia-east1/api_tw_firebase";
+axios.defaults.baseURL = IS_DEV
+  ? "http://localhost:5000/tuango-tw-firebase/asia-east1/api_tw_firebase"
+  : "https://asia-east1-tuango-tw-firebase.cloudfunctions.net/api_tw_firebase";
 // axios.defaults.baseURL =
 //   "https://lhlnrflto9.execute-api.ap-east-1.amazonaws.com";
 axios.defaults.withCredentials = true;
