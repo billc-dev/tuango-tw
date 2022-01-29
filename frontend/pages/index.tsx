499967a51cb2785acabf95c20153785f3c411be0
billchengdev <chengbill0418@gmail.com>
2022-01-29 14:59:00 +0800

    fix: add head for index and conditional head for posts

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,15 @@
 import Button from "components/Core/Button";
 import type { NextPage } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
   const router = useRouter();
   return (
     <div>
+      <Head>
+        <title>開心團購</title>
+      </Head>
       <Button onClick={() => router.push("/posts")}>Posts</Button>
       {/* <Link href="/posts">posts</Link> */}
       <br />
