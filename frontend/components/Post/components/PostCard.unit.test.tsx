a583f02b690a0a9babb9d2af5683f6d050d387eb
billchengdev <chengbill0418@gmail.com>
2022-02-04 09:16:29 +0800

    test: add unit test for PostCard and int test for PostCards

---
+++ frontend/components/Post/components/PostCard.unit.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import { IPostCard } from "types";
+import { expectedPosts } from "__mock__/post.mock";
+import PostCard from "./PostCard";
+
+const renderComponent = () =>
+  render(<PostCard post={expectedPosts.posts[0]} />);
+
+describe("PostCard", () => {
+  let expectedPost: IPostCard;
+
+  beforeEach(() => {
+    expectedPost = expectedPosts.posts[0];
+  });
+
+  it("renders title", () => {
+    const { getByText } = renderComponent();
+    const title = getByText(expectedPost.title);
+    expect(title).toBeInTheDocument();
+  });
+
+  it("renders first image", () => {
+    const { getByAltText } = renderComponent();
+    const image = getByAltText("product");
+    expect(image).toHaveAttribute(
+      "src",
+      // @ts-expect-error
+      expectedPost.imageUrls[0].sm
+    );
+  });
+
+  it("renders seller displayName", () => {
+    const { getByText } = renderComponent();
+    const displayName = getByText(expectedPost.displayName);
+    expect(displayName).toBeInTheDocument();
+  });
+
+  it("renders orderCount", () => {
+    const { getByLabelText } = renderComponent();
+    const orderCount = getByLabelText("orderCount");
+    expect(orderCount).toBeInTheDocument();
+    expect(orderCount.textContent).toEqual(`${expectedPost.orderCount} 訂單`);
+  });
+});
