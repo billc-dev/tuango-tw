7a83267ba72e5429fd939909b96e04308abf7eb1
billchengdev <chengbill0418@gmail.com>
2022-02-04 09:19:37 +0800

    fix: modify routing path

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,5 @@
 import Button from "components/Core/Button";
 import type { NextPage } from "next";
-import Head from "next/head";
 import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
@@ -8,10 +7,8 @@ const Home: NextPage = () => {
   return (
     <div>
       <Button onClick={() => router.push("/posts")}>Posts</Button>
-      {/* <Link href="/posts">posts</Link> */}
       <br />
       <Button onClick={() => router.push("/login")}>Login</Button>
-      {/* <Link href="/login">login</Link> */}
     </div>
   );
 };
