d436889fc37f67c87fef5af1f7e6bbd5f69edd00
billchengdev <chengbill0418@gmail.com>
2022-02-04 09:18:25 +0800

    fix: modify topNavbar z-height

--- frontend/components/Navigation/TopNavbar.tsx
+++ frontend/components/Navigation/TopNavbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const TopNavbar = () => {
   return (
-    <div className="sticky top-0 z-50 flex h-14 items-center bg-white p-3 shadow-md dark:bg-zinc-800">
+    <div className="sticky top-0 flex h-14 items-center bg-white p-3 shadow-md dark:bg-zinc-800">
       top navbar
       <ThemeToggle />
     </div>
