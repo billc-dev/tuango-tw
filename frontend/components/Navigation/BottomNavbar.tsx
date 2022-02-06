a09248e140e33c177e76076c5c4930dc27c88ecb
billchengdev <chengbill0418@gmail.com>
2022-02-06 16:53:10 +0800

    fix: do not return null if id is defined

--- frontend/components/Navigation/BottomNavbar.tsx
+++ frontend/components/Navigation/BottomNavbar.tsx
@@ -1,9 +1,6 @@
-import { useRouter } from "next/router";
 import React from "react";
 
 const BottomNavbar = () => {
-  const router = useRouter();
-  // if (router.query.id) return null;
   return (
     <div className="pt-14">
       <div className="fixed inset-x-0 bottom-0 block rounded-t-2xl bg-white p-3 dark:bg-zinc-800">
