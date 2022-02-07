2d68ab0eb5abce6bc63bc875d6dfc8bbabd97698
billchengdev <chengbill0418@gmail.com>
2022-02-07 21:26:05 +0800

    fix: redirect url

--- frontend/utils/urls.ts
+++ frontend/utils/urls.ts
@@ -1,5 +1,9 @@
 import { nanoid } from "nanoid";
-export const REDIRECT_URL = "http://localhost:3000/redirect";
-export const LINE_LOGIN_URL = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654947889&redirect_uri=${REDIRECT_URL}&state=${nanoid()}&scope=profile%20openid`;
+import { IS_DEV } from "./constants";
+
 export const WINDOW_URL =
   window.location.protocol + "//" + window.location.host;
+export const REDIRECT_URL = IS_DEV
+  ? "http://localhost:3000/redirect"
+  : `${WINDOW_URL}/redirect`;
+export const LINE_LOGIN_URL = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654947889&redirect_uri=${REDIRECT_URL}&state=${nanoid()}&scope=profile%20openid`;
