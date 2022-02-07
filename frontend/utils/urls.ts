9d12756fd899327e16b837a93238de039b75851c
billchengdev <chengbill0418@gmail.com>
2022-02-07 20:42:37 +0800

    feat: implement login mutation

--- frontend/utils/urls.ts
+++ frontend/utils/urls.ts
@@ -1,3 +1,5 @@
 import { nanoid } from "nanoid";
 export const REDIRECT_URL = "http://localhost:3000/redirect";
 export const LINE_LOGIN_URL = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654947889&redirect_uri=${REDIRECT_URL}&state=${nanoid()}&scope=profile%20openid`;
+export const WINDOW_URL =
+  window.location.protocol + "//" + window.location.host;
