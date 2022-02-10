239daa93352d2b3e2c04f33610f62b585a7a3649
billchengdev <chengbill0418@gmail.com>
2022-02-10 16:17:58 +0800

    feat: implement next api auth

--- frontend/components/Navigation/TopNavbar.tsx
+++ frontend/components/Navigation/TopNavbar.tsx
@@ -1,11 +1,16 @@
+import axios from "axios";
 import ThemeButton from "components/Button/ThemeButton";
 import { useUser } from "domain/User/hooks";
-import React from "react";
+import React, { useEffect } from "react";
+import { WINDOW_URL } from "utils/constants";
 
 const TopNavbar = () => {
   const { data, isError } = useUser();
   console.log(data, isError);
-
+  useEffect(() => {
+    const asyncFn = async () => await axios.post(`${WINDOW_URL}/api/auth`);
+    asyncFn();
+  }, []);
   return (
     <div className="sticky top-0 z-10 flex h-14 items-center bg-white p-3 shadow-md dark:bg-zinc-800">
       top navbar
