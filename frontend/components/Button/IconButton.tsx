8b9fccdf24160685da54be67eedb2884c698d6cd
billchengdev <chengbill0418@gmail.com>
2022-02-13 14:03:53 +0800

    style: fix light mode active bg

--- frontend/components/Button/IconButton.tsx
+++ frontend/components/Button/IconButton.tsx
@@ -9,7 +9,7 @@ const IconButton: FC<Props> = ({ children, ...props }) => {
   return (
     <button
       {...props}
-      className="rounded-full p-2 active:bg-zinc-300 disabled:text-gray-300 disabled:hover:bg-transparent disabled:active:bg-transparent sm:hover:bg-zinc-300"
+      className="rounded-full p-2 active:bg-zinc-300 disabled:text-gray-300 disabled:hover:bg-transparent disabled:active:bg-transparent dark:active:bg-zinc-600 sm:hover:bg-zinc-300 sm:dark:hover:bg-zinc-600"
     >
       <div className="h-6 w-6">{children}</div>
     </button>
