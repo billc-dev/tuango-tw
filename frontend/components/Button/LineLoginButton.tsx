6dbb4bc67054de38fe1e49f8580a0838709a4510
billchengdev <chengbill0418@gmail.com>
2022-02-16 21:49:45 +0800

    feat: add redirect url for line login

--- frontend/components/Button/LineLoginButton.tsx
+++ frontend/components/Button/LineLoginButton.tsx
@@ -1,17 +1,23 @@
-import { LINE_LOGIN_URL_WITH_PARAMS } from "domain/User/urls";
+import { LINE_LOGIN_URL_WITH_PARAMS } from "domain/User/services/urls";
 import React, { FC } from "react";
+import { WINDOW_URL } from "utils/constants";
 import Button from ".";
 
 interface Props {
   text?: string;
+  size?: "sm" | "lg";
 }
 
-const LineLoginButton: FC<Props> = ({ text }) => {
+const LineLoginButton: FC<Props> = ({ text, size }) => {
   return (
     <Button
+      variant="primary"
+      size={size}
       onClick={() =>
         window.open(
-          LINE_LOGIN_URL_WITH_PARAMS(`?redirect=${window.location.href}`),
+          window.location.pathname === "/redirect"
+            ? LINE_LOGIN_URL_WITH_PARAMS(`?redirect=${WINDOW_URL}`)
+            : LINE_LOGIN_URL_WITH_PARAMS(`?redirect=${window.location.href}`),
           "_self"
         )
       }
