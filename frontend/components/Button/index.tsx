8ed7deee88601f0c0ba1950220ebf10e67cd842a
billchengdev <chengbill0418@gmail.com>
2022-02-17 14:48:56 +0800

    fix: dynamic classname

--- frontend/components/Button/index.tsx
+++ frontend/components/Button/index.tsx
@@ -37,12 +37,12 @@ const Button: FC<Props> = (props) => {
       return "bg-green-500 hover:bg-green-700";
     } else if (variant === "danger") {
       return "bg-red-500 hover:bg-red-700";
-    } else return "bg-zinc-400";
+    } else return "bg-zinc-400 hover:bg-zinc-600";
   };
   return (
     <div className="p-1">
       <button
-        className={`select-none py-1 px-3 text-white shadow transition hover:bg-zinc-600 disabled:text-gray-300 dark:text-gray-100 ${
+        className={`select-none py-1 px-3 text-white shadow transition disabled:text-gray-300 dark:text-gray-100 ${
           fullWidth ? "w-full" : ""
         } ${variantStyles()} ${size === "lg" ? "px-4 text-lg" : ""} ${
           pill ? "rounded-full" : "rounded-md"
