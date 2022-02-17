36edb1c4ceffa6ed1168aef445278bd89d0ca5c7
billchengdev <chengbill0418@gmail.com>
2022-02-17 11:12:42 +0800

    styles: add danger styles

--- frontend/components/Button/index.tsx
+++ frontend/components/Button/index.tsx
@@ -31,14 +31,21 @@ const Button: FC<Props> = (props) => {
       </>
     );
   };
-  // ${variant === "" && "bg-gray-400 hover:bg-gray-600"}
+  const variantStyles = () => {
+    if (!variant) return "";
+    if (variant === "primary") {
+      return "bg-green-500 hover:bg-green-700";
+    } else if (variant === "danger") {
+      return "bg-red-500 hover:bg-red-700";
+    }
+  };
   return (
     <div className="p-1">
       <button
         className={`select-none bg-zinc-400 py-1 px-3 text-white shadow transition hover:bg-zinc-600 disabled:text-gray-300 dark:text-gray-100 ${
           fullWidth && "w-full"
         } 
-        ${variant === "primary" && "bg-green-500 hover:bg-green-600"}
+        ${variantStyles()}
         ${size === "lg" && "px-4 text-lg"}
         ${pill ? "rounded-full" : "rounded-md"}
         ${className}`}
