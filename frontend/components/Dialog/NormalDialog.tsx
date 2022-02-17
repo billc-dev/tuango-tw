b5780ce385618843826a4b63741e4eb517ce670e
billchengdev <chengbill0418@gmail.com>
2022-02-17 22:10:04 +0800

    style: remove w-full

--- frontend/components/Dialog/NormalDialog.tsx
+++ frontend/components/Dialog/NormalDialog.tsx
@@ -47,7 +47,7 @@ const NormalDialog: FC<Props> = ({ open, setOpen, title, children }) => {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <div className="my-8 inline-block w-full max-w-md transform overflow-hidden rounded-2xl bg-white px-6 py-4 text-left align-middle shadow-xl transition-all">
+            <div className="my-8 inline-block max-w-md transform overflow-hidden rounded-2xl bg-white px-6 py-4 text-left align-middle shadow-xl transition-all">
               <Dialog.Title
                 as="h3"
                 className="text-lg font-medium leading-6 text-gray-900"
