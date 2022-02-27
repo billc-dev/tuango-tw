22baab9f7114f79219a9952dde8caaa096e30916
billchengdev <chengbill0418@gmail.com>
2022-02-27 17:54:10 +0800

    fix: add props to div

--- frontend/components/Card/index.tsx
+++ frontend/components/Card/index.tsx
@@ -1,13 +1,14 @@
-import React, { FC } from "react";
+import React, { FC, HTMLAttributes } from "react";
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
-const Card: FC<Props> = ({ children, className }) => {
+const Card: FC<Props> = ({ children, className, ...props }) => {
   return (
     <div
       className={`mt-2 overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 ${className}`}
+      {...props}
     >
       {children}
     </div>
