e0fd62e4803ca9d24fd74d16a7cbaf099f40cc76
billchengdev <chengbill0418@gmail.com>
2022-02-27 21:36:24 +0800

    refactor: reuse OrderStatus

--- backend/firebase/functions/src/api/order/order.d.ts
+++ backend/firebase/functions/src/api/order/order.d.ts
@@ -1,4 +1,26 @@
 import { Document } from "mongoose";
+
+type OrderStatus =
+  | "ordered"
+  | "delivered"
+  | "completed"
+  | "missing"
+  | "canceled";
+
+interface SingleOrder {
+  id: string;
+  item: string;
+  qty: number;
+  price: number;
+  status: string;
+  location: string;
+  hasName: boolean;
+}
+
+interface OrderHistory {
+  status: OrderStatus;
+  createdAt: string;
+}
 export interface IOrder {
   _id: string;
   orderNum: number;
@@ -19,12 +41,7 @@ export interface IOrder {
   deliveredAt: string;
   canceledAt: string;
   orderHistory: OrderHistory[];
-  status: "ordered" | "delivered" | "completed" | "missing" | "canceled";
-}
-
-interface OrderHistory {
-  status: "ordered" | "delivered" | "completed" | "missing" | "canceled";
-  createdAt: string;
+  status: OrderStatus;
 }
 
 export type MongooseOrder = Document<unknown, any, IOrder> &
