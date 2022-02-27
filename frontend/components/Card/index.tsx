9193a77e75b94934ebad7071601f0874bb726431
billchengdev <chengbill0418@gmail.com>
2022-02-27 17:17:13 +0800

    fix: enable className override

--- frontend/components/Card/index.tsx
+++ frontend/components/Card/index.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from "react";
 
-const Card: FC = ({ children }) => {
+interface Props {
+  className?: string;
+}
+
+const Card: FC<Props> = ({ children, className }) => {
   return (
-    <div className="mt-2 overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800">
+    <div
+      className={`mt-2 overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 ${className}`}
+    >
       {children}
     </div>
   );
