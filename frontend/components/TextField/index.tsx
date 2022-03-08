9025eff56996071c44109f0fd9ca5f28ec8881bb
billchengdev <chengbill0418@gmail.com>
2022-03-08 19:29:56 +0800

    style: add height, error, and color variants

--- frontend/components/TextField/index.tsx
+++ frontend/components/TextField/index.tsx
@@ -1,13 +1,35 @@
 import React, { FC, InputHTMLAttributes } from "react";
 
-const TextField: FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  height?: "tall";
+  error?: string;
+  color?: "grey";
+}
+
+const TextField: FC<Props> = React.forwardRef((props, ref) => {
+  const { height, error, color, ...rest } = props;
   return (
-    <input
-      className="focus:border-line-400 focus:ring-line-400 mb-2 w-full h-16 rounded-lg border py-4 px-3 placeholder-gray-400 focus:ring-1"
-      type="text"
-      {...props}
-    />
+    <>
+      {props.placeholder && (
+        <label htmlFor={props.placeholder} className="block pb-2">
+          {props.placeholder}
+        </label>
+      )}
+      <input
+        ref={ref as any}
+        className={`mb-2 w-full rounded-lg border px-3  placeholder-zinc-400 dark:border-zinc-600 ${
+          height === "tall" ? "h-16" : "h-14"
+        } ${
+          !error
+            ? "focus:border-line-400 focus:ring-line-400 focus:ring-1"
+            : "border-red-500 ring-red-500 ring-1"
+        } ${color === "grey" && "bg-zinc-100 dark:bg-zinc-800"}`}
+        {...rest}
+      />
+    </>
   );
-};
+});
+
+TextField.displayName = "TextField";
 
 export default TextField;
