5c5aa785d8f0d1c244b52b2a1e7d1a50d4faa193
billchengdev <chengbill0418@gmail.com>
2022-03-15 21:37:41 +0800

    feat: create select component

---
+++ frontend/components/Select/index.tsx
@@ -0,0 +1,24 @@
+import React, { FC, SelectHTMLAttributes } from "react";
+
+interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
+  options: { value: string; label: string }[];
+}
+
+const Select: FC<Props> = React.forwardRef((props, ref) => {
+  const { options, className, ...rest } = props;
+  return (
+    <select
+      ref={ref as any}
+      className={`rounded-lg w-full bg-zinc-100 border-zinc-200 dark:bg-zinc-800 dark:border-zinc-600 mb-2 focus:border-line-400 focus:ring-line-400 ${className}`}
+      {...rest}
+    >
+      {options.map((option, index) => (
+        <option key={option.value + index} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+});
+
+export default Select;
