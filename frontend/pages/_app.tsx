b63573bbb9004fbbe7039a172fc7ba686496400d
billchengdev <chengbill0418@gmail.com>
2022-03-17 19:47:48 +0800

    fix: increase stateTime

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -27,7 +27,7 @@ export function App({ Component, pageProps }: AppProps) {
     () =>
       new QueryClient({
         defaultOptions: {
-          queries: { staleTime: 1000 * 60, refetchOnMount: "always" },
+          queries: { staleTime: 1000 * 60 * 3, refetchOnMount: "always" },
         },
       })
   );
