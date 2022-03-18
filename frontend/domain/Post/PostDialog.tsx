9053292643f324ab423860d43f330ab0cd1662de
billchengdev <chengbill0418@gmail.com>
2022-03-18 17:43:29 +0800

    feat: add loading animation

--- frontend/domain/Post/PostDialog.tsx
+++ frontend/domain/Post/PostDialog.tsx
@@ -3,6 +3,7 @@ import React, { FC, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import Dialog from "components/Dialog";
+import AnimatedSpinner from "components/svg/AnimatedSpinner";
 
 import PostActions from "./components/PostActions";
 import PostContent from "./components/PostContent";
@@ -12,11 +13,11 @@ interface Props {
   postId: string;
 }
 
-const PostDialog: FC<Props> = (props) => {
+const PostDialog: FC<Props> = ({ postId }) => {
   const router = useRouter();
-  const { postId } = props;
   const [open, setOpen] = useState(false);
-  const { data } = usePost(postId);
+  const { data, isLoading } = usePost(postId);
+
   const handleClose = () => {
     router.push(
       { query: { ...router.query, postId: "", action: "" } },
@@ -30,16 +31,23 @@ const PostDialog: FC<Props> = (props) => {
     else setOpen(false);
   }, [postId]);
 
-  return data?.post ? (
-    <Dialog
-      open={open}
-      handleClose={() => handleClose()}
-      title={data.post.title}
-    >
-      <PostContent post={data.post} />
-      <PostActions post={data.post} />
-    </Dialog>
-  ) : null;
+  return (
+    <>
+      <div
+        className={`rounded shadow transition-opacity bg-zinc-200 p-1.5 fixed bottom-16 right-3 z-50 ${
+          isLoading ? "opacity-95" : "opacity-0"
+        }`}
+      >
+        {isLoading && <AnimatedSpinner />}
+      </div>
+      {data?.post && (
+        <Dialog open={open} handleClose={handleClose} title={data.post.title}>
+          <PostContent post={data.post} />
+          <PostActions post={data.post} />
+        </Dialog>
+      )}
+    </>
+  );
 };
 
 export default PostDialog;
