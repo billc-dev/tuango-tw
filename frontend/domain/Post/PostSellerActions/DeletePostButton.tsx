dec34ba98a7f2c49a6f3df8259ab27b22be21482
billchengdev <chengbill0418@gmail.com>
2022-03-23 11:06:02 +0800

    feat: add confirm dialog for delete post

--- frontend/domain/Post/PostSellerActions/DeletePostButton.tsx
+++ frontend/domain/Post/PostSellerActions/DeletePostButton.tsx
@@ -3,7 +3,9 @@ import React, { FC, useState } from "react";
 import { TrashIcon } from "@heroicons/react/outline";
 import toast from "react-hot-toast";
 
+import Button from "components/Button";
 import IconButton from "components/Button/IconButton";
+import NormalDialog from "components/Dialog/NormalDialog";
 
 import { useDeletePost } from "../hooks";
 
@@ -13,10 +15,12 @@ interface Props {
 
 const DeletePostButton: FC<Props> = ({ postId }) => {
   const deletePost = useDeletePost();
+  const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const handleDelete = () => {
     setLoading(true);
     toast.loading("刪除中...", { id: "deletePost" });
+    setOpen(false);
     deletePost.mutate(postId, {
       onSettled: () => {
         setLoading(false);
@@ -24,9 +28,25 @@ const DeletePostButton: FC<Props> = ({ postId }) => {
     });
   };
   return (
-    <IconButton loading={loading} onClick={handleDelete}>
-      <TrashIcon className="text-zinc-500" />
-    </IconButton>
+    <>
+      <IconButton loading={loading} onClick={() => setOpen(true)}>
+        <TrashIcon className="text-zinc-500" />
+      </IconButton>
+      <NormalDialog
+        open={open}
+        setOpen={setOpen}
+        title="您確定要取消這筆訂單嗎？"
+      >
+        <div className="flex justify-end gap-2 pt-2">
+          <Button size="lg" variant="danger" onClick={() => handleDelete()}>
+            刪除
+          </Button>
+          <Button size="lg" onClick={() => setOpen(false)}>
+            取消
+          </Button>
+        </div>
+      </NormalDialog>
+    </>
   );
 };
 
