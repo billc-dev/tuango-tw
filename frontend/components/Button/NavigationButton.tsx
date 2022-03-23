50d1b47bd352680e32a3442113fc861255a2c0be
billchengdev <chengbill0418@gmail.com>
2022-03-23 21:30:18 +0800

    fix: change into button

--- frontend/components/Button/NavigationButton.tsx
+++ frontend/components/Button/NavigationButton.tsx
@@ -1,28 +1,30 @@
 import { useRouter } from "next/router";
 import React, { FC, HTMLAttributes } from "react";
 
-interface Props extends HTMLAttributes<HTMLDivElement> {
+interface Props extends HTMLAttributes<HTMLButtonElement> {
   path?: string;
   text: string;
 }
 
-const NavigationButton: FC<Props> = ({ path, text, children, ...props }) => {
+const NavigationButton: FC<Props> = (props) => {
+  const { path, text, children, className, ...rest } = props;
   const router = useRouter();
   const selected = router.pathname === path;
   return (
-    <div
-      className={`flex w-full transform flex-col items-center transition active:scale-[1.1]  ${
+    <button
+      type="button"
+      className={`flex w-full transform flex-col items-center transition py-2 active:bg-zinc-200 dark:active:bg-zinc-700 ${
         selected && "scale-[1.1] text-blue-500"
-      }`}
+      } ${className}`}
       onClick={() => {
         if (!path) return;
         if (!selected) router.push(path);
       }}
-      {...props}
+      {...rest}
     >
       <div className="h-6 w-6">{children}</div>
       <label className="select-none text-xs">{text}</label>
-    </div>
+    </button>
   );
 };
 
