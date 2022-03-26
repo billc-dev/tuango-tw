fd996ddde03ecef3f59bc20d411cf06ff229f6d5
billchengdev <chengbill0418@gmail.com>
2022-03-26 16:12:42 +0800

    style: add blur effect on image load

--- frontend/components/Card/CardAvatar.tsx
+++ frontend/components/Card/CardAvatar.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useState } from "react";
 
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
 
 interface Props {
   img: string;
@@ -10,19 +9,22 @@ interface Props {
 
 const CardAvatar: FC<Props> = ({ img, alt }) => {
   const [error, setError] = useState(false);
+  const [loaded, setLoaded] = useState(false);
   return (
-    <div className="select-none h-10 w-10 rounded-full overflow-hidden">
+    <div className="select-none  overflow-hidden">
       {!error ? (
         <LazyLoadImage
-          effect="blur"
           alt={alt}
-          className="bg-zinc-400 dark:bg-zinc-500"
+          className={`h-10 w-10 rounded-full overflow-hidden transition-all ${
+            loaded ? "opacity-100 blur-0" : "opacity-0 blur-sm"
+          }`}
           src={img}
+          onLoad={() => setLoaded(true)}
           placeholder={<div className="h-10 w-10 rounded-full bg-zinc-400" />}
           onError={() => setError(true)}
         />
       ) : (
-        <div className="flex items-center justify-center rounded-full bg-zinc-400 text-xl text-white dark:bg-zinc-500">
+        <div className="flex items-center justify-center h-10 w-10 rounded-full bg-zinc-400 text-xl text-white dark:bg-zinc-500">
           {alt.substring(0, 1)}
         </div>
       )}
