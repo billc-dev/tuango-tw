c1f0c7e272b81436e1223de7ba601146ba19e003
billchengdev <chengbill0418@gmail.com>
2022-03-27 20:33:31 +0800

    feat: implement CompletedSkeleton

--- frontend/domain/Order/UserOrders/Completed.tsx
+++ frontend/domain/Order/UserOrders/Completed.tsx
@@ -4,32 +4,38 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 import { useCompletedUserOrders } from "../hooks";
 import CompletedCard from "./CompletedCard";
+import CompletedSkeleton from "./CompletedSkeleton";
 
 const Completed = () => {
   const limit = 16;
   const ordersQuery = useCompletedUserOrders(limit, "completed");
+  const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
+    ordersQuery;
   const ordersLength = ordersQuery.data?.pages.reduce(
     (sum, page) => page.data.completes.length + sum,
     0
   );
+
   return (
     <div className="max-w-md mx-auto">
       <InfiniteScroll
         dataLength={ordersLength || 0}
-        next={() => ordersQuery.fetchNextPage()}
-        hasMore={!!ordersQuery.hasNextPage}
+        next={() => fetchNextPage()}
+        hasMore={!!hasNextPage}
         loader={<></>}
       >
-        {ordersQuery.data?.pages.map((page) =>
+        {/* <CompletedSkeleton /> */}
+        {data?.pages.map((page) =>
           page.data.completes.map((complete) => (
             <CompletedCard key={complete._id} complete={complete} />
           ))
         )}
       </InfiniteScroll>
-      {!ordersQuery.isLoading &&
-        ordersQuery.data?.pages[0].data.completes.length === 0 && (
-          <p className="text-center pt-2">您目前沒有已取貨的訂單</p>
-        )}
+      {(isLoading || isFetchingNextPage) &&
+        [...Array(limit)].map((_, index) => <CompletedSkeleton key={index} />)}
+      {!isLoading && data?.pages[0].data.completes.length === 0 && (
+        <p className="text-center pt-2">您目前沒有已取貨的訂單</p>
+      )}
     </div>
   );
 };
