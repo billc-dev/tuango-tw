7cbe50593bb0a8980be6036025e5115dc30f97e5
billchengdev <chengbill0418@gmail.com>
2022-04-05 18:55:03 +0800

    feat: add  notifySetup

--- frontend/pages/chat.tsx
+++ frontend/pages/chat.tsx
@@ -5,12 +5,14 @@ import ChatDialog from "domain/Chat/ChatDialog";
 import RoomList from "domain/Chat/RoomList";
 import PostDialog from "domain/Post/PostDialog";
 import LoginOverlay from "domain/User/LoginOverlay";
+import NotifySetup from "domain/User/NotifySetup";
 
 const Chat: NextPage = () => {
   const router = useRouter();
   const { chatId, postId } = router.query;
   return (
     <>
+      <NotifySetup />
       <LoginOverlay />
       <RoomList />
       {typeof chatId === "string" && <ChatDialog chatId={chatId} />}
