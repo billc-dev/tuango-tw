8dc1ab7a94dec2b7b21c6c6de17c6a0a3ddb2b75
billchengdev <chengbill0418@gmail.com>
2022-04-09 19:29:30 +0800

    fix: seperate into PostImage functional component

---
+++ frontend/domain/Post/components/PostImage.tsx
@@ -0,0 +1,25 @@
+import React, { FC, useState } from "react";
+
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+interface Props {
+  image: string;
+}
+
+const PostImage: FC<Props> = ({ image }) => {
+  const [loaded, setLoaded] = useState(false);
+  return (
+    <div>
+      <LazyLoadImage
+        src={image}
+        placeholder={<div className="h-72 w-full bg-zinc-500" />}
+        className={`max-h-72 object-contain transition-all duration-300 ${
+          loaded ? "opacity-100 blur-0" : "opacity-0 blur-sm"
+        }`}
+        onLoad={() => setLoaded(true)}
+      />
+    </div>
+  );
+};
+
+export default PostImage;
