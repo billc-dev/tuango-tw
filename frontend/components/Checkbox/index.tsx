454c1562392470d6c7cdfe25225d5111a74ce49f
billchengdev <chengbill0418@gmail.com>
2022-04-09 19:06:46 +0800

    feat: add large checkbox size

--- frontend/components/Checkbox/index.tsx
+++ frontend/components/Checkbox/index.tsx
@@ -1,13 +1,22 @@
-import React, { FC, InputHTMLAttributes } from "react";
+import React, { DetailedHTMLProps, FC, InputHTMLAttributes } from "react";
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+interface Props
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  checkboxSize?: "large";
+}
 
 const index: FC<Props> = (props) => {
+  const { checkboxSize, ...rest } = props;
   return (
     <input
       type="checkbox"
-      className="rounded cursor-pointer transition text-line-400 focus:ring-transparent focus:outline-transparent border-zinc-200 dark:border-zinc-600"
-      {...props}
+      className={`rounded cursor-pointer transition text-line-400 focus:ring-transparent focus:outline-transparent border-zinc-200 dark:border-zinc-600 ${
+        checkboxSize === "large" && "p-2"
+      }`}
+      {...rest}
     />
   );
 };
