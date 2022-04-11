f85b614c8d90ace7071d19cd2819f9b4508e04be
billchengdev <chengbill0418@gmail.com>
2022-04-12 01:35:50 +0800

    feat: implement lightbox

--- frontend/domain/Chat/Messages/ImageMessage.tsx
+++ frontend/domain/Chat/Messages/ImageMessage.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useState } from "react";
 
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 import { IMessage } from "../types";
@@ -11,10 +13,12 @@ interface Props {
 
 const ImageMessage: FC<Props> = ({ message, isUserMessage }) => {
   const [loaded, setLoaded] = useState(false);
+  const [open, setOpen] = useState(false);
 
   return (
     <div className={` ${isUserMessage && "order-last"}`}>
       <LazyLoadImage
+        onClick={() => setOpen(true)}
         className={`rounded-xl m-1 object-cover max-h-48 ${
           isUserMessage && "order-last"
         }`}
@@ -24,6 +28,13 @@ const ImageMessage: FC<Props> = ({ message, isUserMessage }) => {
       />
       {!loaded && (
         <div className="rounded-xl animate-pulse bg-zinc-300 h-48 w-48" />
+      )}
+      {open && (
+        // @ts-ignore
+        <Lightbox
+          mainSrc={message.imageUrl?.md || ""}
+          onCloseRequest={() => setOpen(false)}
+        />
       )}
     </div>
   );
