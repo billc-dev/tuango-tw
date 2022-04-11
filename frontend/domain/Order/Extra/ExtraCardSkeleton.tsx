ad153763b029734a46801dcbe5fb991a2ed0b5f7
billchengdev <chengbill0418@gmail.com>
2022-04-11 17:00:36 +0800

    style: change card shadow

--- frontend/domain/Order/Extra/ExtraCardSkeleton.tsx
+++ frontend/domain/Order/Extra/ExtraCardSkeleton.tsx
@@ -4,7 +4,7 @@ import Card from "components/Card";
 
 const ExtraCardSkeleton = () => {
   return (
-    <Card className="pr-2 grid grid-cols-3 gap-2 shadow-lg bg-white ring-1 ring-zinc-200 dark:ring-0 animate-pulse">
+    <Card className="pr-2 grid grid-cols-3 gap-2 shadow hover:shadow-lg transition-shadow bg-white ring-1 ring-zinc-200 dark:ring-0 animate-pulse">
       <div className="col-span-1">
         <div className="h-28 w-full bg-zinc-300 dark:bg-zinc-700" />
       </div>
