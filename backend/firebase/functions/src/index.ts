37ea72ef7d2619ade8d8f31707fc23d66e614a67
billchengdev <chengbill0418@gmail.com>
2022-04-12 16:51:14 +0800

    feat: implement pingVercel and pingFirebase

--- backend/firebase/functions/src/index.ts
+++ backend/firebase/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as functions from "firebase-functions";
 
+import axios from "axios";
 import * as compression from "compression";
 import * as cookieParser from "cookie-parser";
 import * as cors from "cors";
@@ -59,6 +60,32 @@ export const api_tw_firebase = functions
   .region("asia-east1")
   .runWith({ timeoutSeconds: 10 })
   .https.onRequest(app);
+
+export const pingVercel = functions.pubsub
+  .schedule("every 2 minutes")
+  .onRun(async () => {
+    try {
+      await axios.get(
+        "https://xn--ndsp5rmr3blfh.com/posts?postId=6254430bf63a6c33a8a22b5a"
+      );
+    } catch (error) {
+      console.log(error);
+    }
+    return null;
+  });
+
+export const pingFirebase = functions.pubsub
+  .schedule("every 2 minutes")
+  .onRun(async () => {
+    try {
+      await axios.get(
+        "https://asia-east1-tuango-tw-firebase.cloudfunctions.net/api_tw_firebase/posts/paginate/initial?limit=20"
+      );
+    } catch (error) {
+      console.log(error);
+    }
+    return null;
+  });
 
 // firebase functions:config:get > .runtimeconfig.json
 // firebase functions:config:set mongodb_uri.post_dev="<url>"
--- backend/firebase/functions/src/utils/constant.ts
+++ backend/firebase/functions/src/utils/constant.ts
@@ -1,2 +1,3 @@
-export const IS_DEV = process.env.NODE_ENV !== "production";
+// export const IS_DEV = process.env.NODE_ENV !== "production";
+export const IS_DEV = true;
 export const indexAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
