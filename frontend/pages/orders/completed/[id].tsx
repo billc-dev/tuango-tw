8707ed52e2e4b70f1660c65c9c3fa6dc4cacfc58
billchengdev <chengbill0418@gmail.com>
2022-04-17 22:07:46 +0800

    feat: implement view single complete

---
+++ frontend/pages/orders/completed/[id].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+import CompletedCard from "domain/Order/UserOrders/CompletedCard";
+import { useComplete } from "domain/Order/hooks";
+
+const ID = () => {
+  const router = useRouter();
+  const completeQuery = useComplete(router.query.id as string);
+  if (!completeQuery.data) return null;
+  return <CompletedCard complete={completeQuery.data} />;
+};
+
+export default ID;
