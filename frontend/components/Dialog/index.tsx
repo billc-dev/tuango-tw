14109c1c652753b0a6a2fe20152f3e6aac4a99bc
billchengdev <chengbill0418@gmail.com>
2022-04-19 21:12:40 +0800

    fix: add className to DialogProps

--- frontend/components/Dialog/index.tsx
+++ frontend/components/Dialog/index.tsx
@@ -9,10 +9,11 @@ interface DialogProps {
   handleClose: Function;
   title: string;
   id?: string;
+  className?: string;
 }
 
 const Dialog: FC<DialogProps> = (props) => {
-  const { children, open, handleClose, title, id } = props;
+  const { children, open, handleClose, title, id, className } = props;
   const [animate, setAnimate] = useState(false);
   useEffect(() => {
     if (open) {
@@ -29,7 +30,7 @@ const Dialog: FC<DialogProps> = (props) => {
       open={open}
       className={`fixed top-0 left-0 inset-0 z-10 h-full w-full overflow-x-hidden overscroll-y-auto bg-white p-0 transition-all duration-300 dark:bg-zinc-900 ${
         animate ? "opacity-100" : "opacity-0"
-      }`}
+      } ${className}`}
     >
       <div className="sticky top-0 z-10 flex w-full items-center bg-white p-1 shadow dark:bg-zinc-800">
         <IconButton onClick={() => handleClose()}>
