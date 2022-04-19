e6e229cea1bec7544d7dbaf40cc7d89997d32245
billchengdev <chengbill0418@gmail.com>
2022-04-19 10:46:58 +0800

    fix: change url and frequency

--- backend/scheduled/functions/src/index.ts
+++ backend/scheduled/functions/src/index.ts
@@ -3,12 +3,11 @@ import * as functions from "firebase-functions";
 
 export const pingVercel = functions
   .region("asia-east1")
-  .pubsub.schedule("every 2 minutes")
+  .pubsub.schedule("every minute")
   .onRun(async () => {
     try {
       await axios.get(
-        "https://tuango-tw.vercel.app/posts?postId=6254430bf63a6c33a8a22b5a"
-        // "https://xn--ndsp5rmr3blfh.com/posts?postId=6254430bf63a6c33a8a22b5a"
+        "https://xn--ndsp5rmr3blfh.com/posts?postId=6254430bf63a6c33a8a22b5a"
       );
     } catch (error) {
       console.log(error);
