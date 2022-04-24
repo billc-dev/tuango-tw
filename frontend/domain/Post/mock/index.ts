dd443761c31b59f325cc88c007cb11f2726304ef
billchengdev <chengbill0418@gmail.com>
2022-04-25 00:01:32 +0800

    test: add mock data

---
+++ frontend/domain/Post/mock/index.ts
@@ -0,0 +1,77 @@
+import { IPost } from "../types";
+
+export const posts: IPost[] = [
+  {
+    _id: "624fa5461a282fa73f1f911d",
+    imageUrls: [
+      {
+        sm: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/KkMwyhGAHRrYlSQVizBVb/sm.jpeg",
+        md: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/KkMwyhGAHRrYlSQVizBVb/md.jpeg",
+        lg: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/KkMwyhGAHRrYlSQVizBVb/md.jpeg",
+      },
+      {
+        sm: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/UdKDdJWVjlVDFXtn9Ca5_/sm.jpeg",
+        md: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/UdKDdJWVjlVDFXtn9Ca5_/md.jpeg",
+        lg: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/UdKDdJWVjlVDFXtn9Ca5_/md.jpeg",
+      },
+      {
+        sm: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/F0LIEETYQywWs-IGcjsYY/sm.jpeg",
+        md: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/F0LIEETYQywWs-IGcjsYY/md.jpeg",
+        lg: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/F0LIEETYQywWs-IGcjsYY/md.jpeg",
+      },
+      {
+        sm: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/yxNnZK-ziALNXIOmJJWpz/sm.jpeg",
+        md: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/yxNnZK-ziALNXIOmJJWpz/md.jpeg",
+        lg: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/yxNnZK-ziALNXIOmJJWpz/md.jpeg",
+      },
+      {
+        sm: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/X4v4JSVAupRvj4UfeMxPL/sm.jpeg",
+        md: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/X4v4JSVAupRvj4UfeMxPL/md.jpeg",
+        lg: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/X4v4JSVAupRvj4UfeMxPL/md.jpeg",
+      },
+      {
+        sm: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/ldydcdzftktduG_FNl-WQ/sm.jpeg",
+        md: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/ldydcdzftktduG_FNl-WQ/md.jpeg",
+        lg: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/ldydcdzftktduG_FNl-WQ/md.jpeg",
+      },
+      {
+        sm: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/MH1dyCCFFwYAWkYXAOaQU/sm.jpeg",
+        md: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/MH1dyCCFFwYAWkYXAOaQU/md.jpeg",
+        lg: "https://d2lduww19xwizo.cloudfront.net/5fefb4739fbc8c6bdf5bc817/MH1dyCCFFwYAWkYXAOaQU/md.jpeg",
+      },
+    ],
+    likeCount: 0,
+    commentCount: 0,
+    orderCount: 24,
+    status: "closed",
+    postNum: 21400,
+    userId: "U3905715b6a65669bc3b1057401761dbe",
+    displayName: "王美娜",
+    pictureUrl:
+      "https://profile.line-scdn.net/0hSl5sgvgVDEMICiZfRN9zFDRPAi5_JAoLcGQULH4CU3t1PkhHYG1LJnkLBXYgaB8cPTlGLShaVXol/small",
+    title: "夜釣小卷&好喝四神湯",
+    deadline: "2022-04-11",
+    deliveryDate: "",
+    body: "夜釣小卷&豬肚四神湯\n🔎#鴻綺 #夜釣小卷 #限量500包 #剛剛好-小資族1餐～ \n\n（A）夜釣小卷：每包2隻，重200+-10%克公克，團購價：[105]元\n\n 鴻綺水產的夜釣小卷，是專門挑選至遠洋(印尼)進口！每隻都是在深夜中用魚竿吊起或是用網子捕撈，活體時就將小卷急速冷凍！保持新鮮！鴻綺水產的小卷有別於市場上泡水過的小卷，而是稍微退冰過後直接包裝，冷凍出貨！並不會大量增加水重，趁機偷斤減兩！\n鴻綺建議：不管清燙、蒸煮炒炸都可以，相當有變化的一個食材！(川燙就很鮮甜！)\n\n💞保存期限：180天\n💞保存方法：-18度冷凍\n--------------------------------------------------------------------\n🔎 #鴻綺#  限量160盒 #豬肚四神湯#漲價了！\n\n（B）豬肚四神湯:每盒重1000克-固體量180克，團購價：[140]元\n\n嚐鮮～很有口碑.真材實料的四神湯！聽說在地人推薦～遊客、在地人必吃的肉圓店，每天都人潮滿滿四神湯想吃加熱就有，大熱天不用出門，很方便！🐷豬肚四神湯：微波加熱、電鍋加熱就可以吃了！方便、健康又美味\n\n💞產地：台灣\n💞保存期限：180天\n💞保存方法：-18度c冷凍\n\n🌟🌟🌟此兩檔皆是鴻綺熱賣商品，組數不多，要買要快！團友下單，團主同時跟廠商下單，所以盡量可加單勿刪單，批商喊結單...團主就跟著結囉！後面喊的就無法確定吃到單-不好意思~~請見諒！！",
+    items: [
+      {
+        itemQty: 83,
+        _id: "624fa5461a282f1b5f1f911e",
+
+        item: "夜釣小卷",
+        price: 105,
+        id: "A",
+      },
+      {
+        itemQty: 80,
+        _id: "624fa5461a282fdcce1f911f",
+
+        item: "豬肚四神湯",
+        price: 140,
+        id: "B",
+      },
+    ],
+    storageType: "frozen",
+    createdAt: "2022-04-08T03:00:22.668Z",
+  },
+];
