f196777de53d3719f0c0a6598f51f66697c24537
billchengdev <chengbill0418@gmail.com>
2022-05-04 10:48:24 +0800

    feat: add route buttons

--- frontend_admin/components/Navigation/TopNavbar.tsx
+++ frontend_admin/components/Navigation/TopNavbar.tsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 import { useQueryClient } from "react-query";
 
 import Button from "components/Button";
+import Menu from "components/Menu";
 import { useMutateLogout, useUser } from "domain/User/hooks";
 import { getLocalStorageUser } from "domain/User/services";
 
 const TopNavbar = () => {
+  const router = useRouter();
   const queryClient = useQueryClient();
   const logout = useMutateLogout();
-
+  const [open, setOpen] = useState(false);
   const { refetch } = useUser();
   useEffect(() => {
     const user = getLocalStorageUser();
@@ -18,16 +21,45 @@ const TopNavbar = () => {
     refetch();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
+  const handleClick = (path: string) => {
+    router.push(path);
+    setOpen(false);
+  };
   return (
     <div className="sticky top-0 h-14 z-10 select-none bg-white py-1 px-1 shadow-md dark:bg-zinc-800">
-      <div className="flex items-center justify-between py-1">
-        <div className="pl-2">
-          <Button
-            onClick={() => {
-              logout.mutate();
-            }}
-          >
+      <div className="flex justify-between items-center h-full ml-2">
+        <div className="flex">
+          <Button variant="inherit" onClick={() => router.push("/")}>
+            取貨
+          </Button>
+          <Button variant="inherit" onClick={() => router.push("/deliver")}>
+            進貨
+          </Button>
+          <Button variant="inherit" onClick={() => router.push("/message")}>
+            訊息
+          </Button>
+          <div className="">
+            <Button variant="inherit" onClick={() => setOpen(true)}>
+              管理
+            </Button>
+            <Menu
+              open={open}
+              handleClose={() => setOpen(false)}
+              items={[
+                { text: "貼文管理", onClick: () => handleClick("/posts") },
+                { text: "訂單管理", onClick: () => handleClick("/orders") },
+                { text: "位置管理", onClick: () => handleClick("/location") },
+                { text: "用戶管理", onClick: () => handleClick("/users") },
+              ]}
+            />
+          </div>
+
+          {/* <Button variant="inherit" onClick={() => router.push("/pickup")}>
+          取貨
+        </Button> */}
+        </div>
+        <div>
+          <Button variant="inherit" onClick={() => logout.mutate()}>
             登出
           </Button>
         </div>
