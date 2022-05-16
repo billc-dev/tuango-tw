67c3b55534fb7d207558b0eb5cc801f7044704fe
billchengdev <chengbill0418@gmail.com>
2022-05-16 18:43:29 +0800

    feat: add page select

--- frontend_admin/components/Table/TablePagination.tsx
+++ frontend_admin/components/Table/TablePagination.tsx
@@ -3,6 +3,7 @@ import React, { FC } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 
 import IconButton from "components/Button/IconButton";
+import Select from "components/Select";
 
 interface Props {
   page: number;
@@ -12,15 +13,29 @@ interface Props {
   onPreviousPage: () => void;
   onNextPage: () => void;
   isLoading?: boolean;
+  onSetPage: (page: number) => void;
 }
 
 const TablePagination: FC<Props> = (props) => {
-  const { page, length, limit, hasNextPage } = props;
-  const { onPreviousPage, onNextPage, isLoading } = props;
+  const { page, length, limit, hasNextPage, isLoading } = props;
+  const { onPreviousPage, onNextPage, onSetPage } = props;
+  const pages = [...Array(Math.ceil(length / limit))].map((_, i) => ({
+    value: i,
+    label: `第${i + 1}頁`,
+  }));
   return (
     <div className="flex justify-end items-center mt-2">
       <div>
-        {page * limit + 1} - {length + page * limit} 之 {length}
+        <Select
+          className="w-20"
+          value={page}
+          options={pages}
+          onChange={(e) => onSetPage(Number(e.target.value))}
+        />
+      </div>
+      <div>
+        {page * limit + 1} - {Math.min(page * limit + limit, length)} 之{" "}
+        {length}
       </div>
       <IconButton disabled={page <= 0} onClick={onPreviousPage}>
         <ChevronLeftIcon />
