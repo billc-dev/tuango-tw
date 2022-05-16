532593c3d3191b9bb5c6aab442990a0222e8fa7d
billchengdev <chengbill0418@gmail.com>
2022-05-16 18:43:40 +0800

    feat: spread table props

--- frontend_admin/components/Table/Table.tsx
+++ frontend_admin/components/Table/Table.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { FC } from "react";
 
-interface Props {
+interface Props
+  extends React.DetailedHTMLProps<
+    React.TableHTMLAttributes<HTMLTableElement>,
+    HTMLTableElement
+  > {
   children?: React.ReactNode;
 }
-const Table: FC<Props> = ({ children }) => {
-  return <table className="table-auto w-full mx-auto">{children}</table>;
+const Table: FC<Props> = ({ children, className, ...props }) => {
+  return (
+    <table className={`table-auto w-full mx-auto ${className}`} {...props}>
+      {children}
+    </table>
+  );
 };
 
 export default Table;
