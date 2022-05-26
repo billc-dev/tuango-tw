75b44962861424eca453eb0fd7144d191384daf8
billchengdev <chengbill0418@gmail.com>
2022-05-26 14:32:25 +0800

    feat: add super buy nav button

--- frontend_fb/components/Navigation/BottomNavbar.tsx
+++ frontend_fb/components/Navigation/BottomNavbar.tsx
@@ -5,8 +5,8 @@ import React from "react";
 import {
   ChatIcon,
   ClipboardListIcon,
+  CurrencyDollarIcon,
   DocumentAddIcon,
-  HomeIcon,
   SearchIcon,
 } from "@heroicons/react/outline";
 
@@ -33,8 +33,12 @@ const BottomNavbar = () => {
       <div className="select-none pt-14 relative">
         <div className="fixed z-[5] inset-x-0 -bottom-0.5 rounded-t-2xl bg-white ring-1 ring-zinc-400 dark:ring-0 dark:bg-zinc-800">
           <div className="mx-auto flex max-w-sm justify-around">
-            <NavigationButton path="/posts" text="首頁">
-              <HomeIcon />
+            <NavigationButton path="/super-buy" text="超便宜團購">
+              <CurrencyDollarIcon />
+            </NavigationButton>
+            <NavigationButton path="/posts" text="開心團購">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img alt="logo" src="/logo.png" />
             </NavigationButton>
             {isSeller && (
               <NavigationButton
