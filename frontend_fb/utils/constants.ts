35418a8e99efaefa58e347cb4725e7cb9e7667e9
billchengdev <chengbill0418@gmail.com>
2022-05-26 14:32:32 +0800

    fix: change api url

--- frontend_fb/utils/constants.ts
+++ frontend_fb/utils/constants.ts
@@ -9,8 +9,8 @@ export const WINDOW_URL = isClient
 export const indexAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 export const API_URL = IS_DEV
-  ? "http://localhost:5000/tuango-tw-firebase/asia-east1/api_tw_firebase"
-  : "https://asia-east1-tuango-tw-firebase.cloudfunctions.net/api_tw_firebase";
+  ? "http://localhost:5000/tuango-tw-firebase/asia-east1/api_fb_firebase"
+  : "https://asia-east1-tuango-tw-firebase.cloudfunctions.net/api_fb_firebase";
 
 export const FB_LOGIN_URL = () =>
   `https://www.facebook.com/v13.0/dialog/oauth?client_id=${process.env.NEXT_PUBLIC_FB_APP_ID}&state=123ab&redirect_uri=${WINDOW_URL}/redirect`;
