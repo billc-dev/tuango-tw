75a95d37615a21a94de3352ecac29c03e2085936
billchengdev <chengbill0418@gmail.com>
2022-05-29 15:34:21 +0800

    fix: set db timeout options

--- backend/firebase/functions/src/utils/constant.ts
+++ backend/firebase/functions/src/utils/constant.ts
@@ -1,7 +1,12 @@
+import { ConnectOptions } from "mongoose";
+
 export const IS_DEV = process.env.NODE_ENV !== "production";
+// export const IS_DEV = true;
 export const indexAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-export const DB_OPTIONS = {
-  maxIdleTimeMS: 270000,
-  minPoolSize: 2,
-  maxPoolSize: 5,
+export const DB_OPTIONS: ConnectOptions = {
+  // maxIdleTimeMS: 270000,
+  connectTimeoutMS: 270000,
+  serverSelectionTimeoutMS: 270000,
+  // minPoolSize: 2,
+  // maxPoolSize: 5,
 };
