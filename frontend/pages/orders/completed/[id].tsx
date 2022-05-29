ab0375e0f5183dfb2b58982a7a28edc781076c1e
billchengdev <chengbill0418@gmail.com>
2022-05-29 15:39:27 +0800

    fix: add postDialog to single completed order

--- frontend/pages/orders/completed/[id].tsx
+++ frontend/pages/orders/completed/[id].tsx
@@ -3,16 +3,19 @@ import React from "react";
 
 import CompletedCard from "domain/Order/UserOrders/CompletedCard";
 import { useComplete } from "domain/Order/hooks";
+import PostDialog from "domain/Post/PostDialog";
 
 const ID = () => {
   const router = useRouter();
+  const { postId } = router.query;
   const completeQuery = useComplete(router.query.id as string);
   if (!completeQuery.data) return null;
   return (
-    <>
-      <p>{completeQuery.data.displayName}</p>
+    <div className="max-w-sm">
+      <p className="mx-6 mt-2">{completeQuery.data.displayName}</p>
       <CompletedCard complete={completeQuery.data} />
-    </>
+      {typeof postId === "string" && <PostDialog postId={postId} />}
+    </div>
   );
 };
 
