546434c0d765407dd1f52900fe68f1b957581bbb
billchengdev <chengbill0418@gmail.com>
2022-06-01 22:47:03 +0800

    style: remove ml

--- frontend_admin/domain/Deliver/DeliverHistory/index.tsx
+++ frontend_admin/domain/Deliver/DeliverHistory/index.tsx
@@ -7,7 +7,7 @@ import DeliverHistoryList from "./DeliverHistoryList";
 const DeliverHistory = () => {
   const [open, setOpen] = React.useState(false);
   return (
-    <div className="my-2 -ml-1">
+    <div className="my-2">
       <Button variant="primary" fullWidth onClick={() => setOpen(!open)}>
         進貨紀錄
       </Button>
