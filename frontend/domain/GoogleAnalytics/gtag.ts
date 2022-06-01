911b6932f8d074e8ca223c33d9c82c9bb5225a5d
billchengdev <chengbill0418@gmail.com>
2022-06-01 16:04:48 +0800

    fix: send all gtag params

--- frontend/domain/GoogleAnalytics/gtag.ts
+++ frontend/domain/GoogleAnalytics/gtag.ts
@@ -10,11 +10,8 @@ export const pageview = (url: URL) => {
   });
 };
 
-export const event = (
-  action: Gtag.EventNames,
-  { event_category, event_label, value }: Gtag.EventParams
-) => {
-  window.gtag("event", action, { event_category, event_label, value });
+export const event = (action: Gtag.EventNames, params: Gtag.EventParams) => {
+  window.gtag("event", action, params);
 };
 
 export const purchaseEvent = (order: IOrder) => {
